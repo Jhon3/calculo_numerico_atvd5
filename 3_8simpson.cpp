445953232ca1c518f2c7a3c36b67ae1b2afee7a6
Daniel Marx <danielmarx08@gmail.com>
2018-05-03 19:35:28 -0300

    Correção no cálculo da regra 3_8 Simpson

--- 3_8simpson.cpp
+++ 3_8simpson.cpp
@@ -3,17 +3,15 @@
 #include <stdlib.h>
 #include <math.h>
 
-
-
 double funcao(double );
 double (*func) (double) = funcao; //Ponteiro para a função
-double tres_oito_simpson(double , double , int , double (*func)(double)); //Método 1/3 simpson
+double tres_oito_simpson(double , double , int , double (*func)(double)); //Método 3/8 simpson
 
 int main()
 {
 	//Pre-definindo o numero de iteracoes e os intervalos,
 	//sem interacao com o usuario.
-	int n = 100000; // numero de sub intervalos (tem que ser par)
+	int n = 6; // numero de sub intervalos (tem que ser par)
 	double a = -5;
 	double b = 5;
 
@@ -51,19 +49,21 @@ double tres_oito_simpson(double a, double b, int n, double (*func)(double))
 		vet_y[i] = func(vet_x[i]);
 	}
 	
-	double result = vet_y[0];
-	for(int i = 1; i < n+1; ++i)
+	//Ex de 3/8 Simpson composta para 7 pontos(Ou seja, aplicada duas vezes):
+	//3h/8 * [ f(x0) + 3*f(x1) + 3*f(x2) + 2*f(x3) + 3*f(x4) + 3*f(x5) + f(x6) ]
+
+	double result = vet_y[0] + vet_y[n];
+	for(int i = 1; i < n; ++i)
 	{
-		if(i%3 == 1)
+		if(i%3 == 0) //Se o índice é multiplo de 3, multiplica f[i] por 2
 		{
 			result = result + 2 * vet_y[i];
 		}
-		else
+		else //Se não, multiplica por 3
 		{
 			result = result + 3 * vet_y[i];
 		}
 	}
-	result = (3*h/8) * (result + vet_y[n+1]);
+	result = (3*h/8) * result;
 	return result;
-	
 }
